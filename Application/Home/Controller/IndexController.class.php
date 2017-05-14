027e89f9c4542bb81dd5efc30e5eed2788a16a48
lilingyuan <1542644868@qq.com>
2017-05-14 15:06:17 +0800

    新增Index控制器

---
+++ Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+/**
+ * 商城首页Controller
+ */
+class IndexController extends Controller{
+    /**
+     * 首页
+     */
+    public function index(){
+        echo 'blog';
+    }
+
+}
+
