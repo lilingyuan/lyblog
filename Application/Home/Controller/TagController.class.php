d128fa45b951f8b832c22417f201237a04bc8b05
lilingyuan <1542644868@qq.com>
2017-05-17 16:24:35 +0800

    控制器

---
+++ Application/Home/Controller/TagController.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2017/5/14
+ * Time: 15:36
+ */
+class IndexController extends Controller{
+    public function index(){
+        echo 555;
+    }
+}
