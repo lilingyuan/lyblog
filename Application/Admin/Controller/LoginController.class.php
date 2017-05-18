091e95ab6b1c48ef56c86a624b821a04b37bf5e4
lilingyuan <1542644868@qq.com>
2017-05-18 17:52:25 +0800

    载入

---
+++ Application/Admin/Controller/LoginController.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2017/5/18
+ * Time: 17:47
+ */
+
+namespace Admin\Controller;
+use Think\Controller;
+
+
+class LoginController extends Controller{
+    public function login(){
+        $this->display();
+    }
+}
