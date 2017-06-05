bcc624570ee1507311a9cb13b9a6c1c01f438747
lilingyuan <1542644868@qq.com>
2017-06-05 22:06:14 +0800

    Update LoginController.class.php

--- Application/Admin/Controller/LoginController.class.php
+++ Application/Admin/Controller/LoginController.class.php
@@ -7,12 +7,7 @@ use Common\Controller\PublicBaseController;
 class LoginController extends PublicBaseController{
     // 登陆页面
     public function login(){
-        // 判断第三方登陆账号中是否有admin
-        $oauth_has_admin=M('Oauth_user')->where(array('is_admin'=>1))->count();
-        // 如果有第三方账号有admin  则要求用第三方登陆 否则用常规登陆
-        if ($oauth_has_admin) {
-            die('请在前台页面通过第三方账号登陆');
-        }
+        
         if(IS_POST){
             $data=I('post.');
             if(check_verify($data['verify'])){
