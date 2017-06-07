0e986d373d18695a90fb9a50a9360b5715ad882f
lilingyuan <1542644868@qq.com>
2017-06-07 19:18:43 +0800

    Update BaseController.class.php

--- Application/Common/Controller/BaseController.class.php
+++ Application/Common/Controller/BaseController.class.php
@@ -1,12 +1,7 @@
 <?php
 namespace Common\Controller;
 use Think\Controller;
-/**
- * Created by PhpStorm.
- * User: lenovo
- * Date: 2017/5/21
- * Time: 16:36
- */
+
 class BaseController extends Controller{
     public function _initialize(){
         
